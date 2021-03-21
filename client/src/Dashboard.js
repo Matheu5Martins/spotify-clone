0f195cd6cf5e6aad937be3462b4fec36bfec325a
Matheu5Martins <dev.sincroni@gmail.com>
2021-03-21 13:32:50 -0300

    spotify clone

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -1,12 +1,29 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import { Container, Form } from 'react-bootstrap'
 import useAuth from './useAuth'
+import SpotifyWebApi from 'spotify-web-api-node'
+
+const spotifyApi = new SpotifyWebApi({
+    clientId: '4f41ca55a526466b85d9386617bbb1e8'
+})
 
 function Dashboard({ code }) {
-    const access_token = useAuth(code)
+    const accessToken = useAuth(code)
+    const [search, setSearch] = useState("")
+
+    useEffect(() => {
+        if (!accessToken) {
+            return
+        }
+        spotifyApi.setAccessToken(accessToken)
+    }, [accessToken])
+
     return (
-        <div>
-            {code}
-        </div>
+        <Container className="d-flex flex-column py-2" style={{ height: "100vh" }}>
+            <Form.Control type="search" placeholder="Search Songs/Artists" value={search} onChange={e => setSearch(e.target.value)} />
+            <div className="flex-grow-1 my-2" style={{ overflowY: "auto" }}>Songs</div>
+            <div>Bottom</div>
+        </Container>
     )
 }
 
--- client/src/useAuth.js
+++ client/src/useAuth.js
@@ -14,30 +14,29 @@ function useAuth(code) {
             setRefreshToken(res.data.refreshToken)
             setExpiresIn(res.data.expiresIn)
             window.history.pushState({}, null, '/')
-        }).catch(() => {
+        }).catch(err => {
+            console.log(err)
             window.location = '/'
         })
     }, [code])
 
     useEffect(() => {
-        if(!refreshToken || !expiresIn){
+        if (!refreshToken || !expiresIn) {
             return
         }
         const interval = setInterval(() => {
-            axios.post('http://192.168.1.110:3333/refresh', {
-            refreshToken
-        }).then(res => {
-            setAccessToken(res.data.accessToken)
-            setExpiresIn(res.data.expiresIn)
-        }).catch(() => {
-            window.location = '/'
-        })
+            axios.post('', {
+                refreshToken
+            }).then(res => {
+                setAccessToken(res.data.accessToken)
+                setExpiresIn(res.data.expiresIn)
+            }).catch(() => {
+                //window.location = '/'
+            })
         }, ((expiresIn - 60) * 1000))
 
         return () => clearInterval(interval)
     }, [refreshToken, expiresIn])
-        
-        
 
     return accessToken
 }
